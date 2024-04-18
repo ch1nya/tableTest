61d31ebb4b3b06af003891d9c47d09cd8f3f111b
ch1nya <you@example.com>
2024-04-18 19:09:02 +0300

    to deploy

--- src/components/ModalBody.jsx
+++ src/components/ModalBody.jsx
@@ -6,10 +6,19 @@ import s from'../styles/ModalBody.module.css'
 
 const ModalBody = ({setCustomizedDataSourse,modalTableState,setModalTableState,isAdding,setIsAdding,tableData,setCount,count,dataSource,setDataSource,setDrawerTableState,drawerTableState}) => {
     const { token } = theme.useToken();
+    // Вылетает ошибка при комбинировании инлайн и модульных стилей, поэтому написано так
     const containerStyle = {
         background: token.colorFillAlter,
         border: `1px solid ${token.colorBorderSecondary}`,
         borderRadius: token.borderRadiusLG,
+        position: "relative",
+        height: "fit-content",
+        width: "100%",
+        padding: 0,
+        overflow: "hidden",
+        fontSize: "10px",
+        display: "flex",
+
     };
 
     const handleOk = () => {
@@ -40,7 +49,9 @@ const ModalBody = ({setCustomizedDataSourse,modalTableState,setModalTableState,i
         };
     }, []);
     return (
-        <div style={s.ModalContainer}>
+        <div
+            // style={s.ModalContainer}
+        >
             <Modal
                 open={isAdding}
                 onOk={handleOk}
@@ -54,7 +65,9 @@ const ModalBody = ({setCustomizedDataSourse,modalTableState,setModalTableState,i
                 keyboard={true}
                 width={'35rem'}
             >
-                <div style={{...s.ModalDrawerContainer,...containerStyle }}>
+                <div
+                    style={containerStyle}
+                >
                     <ModalTable setModalTableState={setModalTableState}
                                 setDrawerIsOpen={setDrawerIsOpen}
                                 dataSource={tableData}/>
